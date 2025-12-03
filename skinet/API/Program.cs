6c6e428d3a2a27c7aeae2d30c9d41dc16a88407e
Mariano Arias <arias.mariano.ar@gmail.com>
2025-12-02 23:13:00 -0300

    clean api project

--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -3,22 +3,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+// de aqui para arriba se considera Service
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+// de aqui para abajo se considera Middleware
 
 app.MapControllers();
 
