11d496973da8994bcbba1da7a48f3bdd1aa489cc
Mariano Arias <arias.mariano.ar@gmail.com>
2025-12-02 23:42:12 -0300

    first entities and setting up EntityFramework

--- skinet/API/Program.cs
+++ skinet/API/Program.cs
@@ -1,8 +1,15 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // de aqui para arriba se considera Service
 var app = builder.Build();
